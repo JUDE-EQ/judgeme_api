d85d16f9bba66fb62ce37f14ab418dac14698629
JUDE-EQ <jude.salvador@eqlink.com>
2022-01-05 04:07:13 +0800

    inital commit

---
+++ netlify/functions/judgeme-api.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body);
+
+  const data = await axios({
+    url: `https://judge.me/api/v1/products/${eventBody.external_id}`,
+    method: "GET",
+    data: {
+      shop_domain: "shopd4.myshopify.com",
+      api_token: "yxHawPYN3RFOtzH7W_dkgSeVJP4",
+      external_id: eventBody.external_id,
+    },
+  })
+    .then((response) => {
+      let product = response.data.product;
+
+      return axios({
+        url: `https://judge.me/api/v1/reviews/`,
+        method: "GET",
+        data: {
+          shop_domain: "shopd4.myshopify.com",
+          api_token: "yxHawPYN3RFOtzH7W_dkgSeVJP4",
+          product_id: product.id,
+        },
+      });
+    })
+    .then((response) => {
+      return response.data.reviews;
+    });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Content-Type",
+      "Access-Control-Allow-Methods": "GET, POST, OPTION",
+    },
+  };
+};
